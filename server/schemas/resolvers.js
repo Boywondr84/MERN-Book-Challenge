2a1b10403c49abb2706cba40d4a3976104b90b64
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-10-17 15:45:24 -0500

    setting up apollo server. Error query.mutation but no mutation in schema, but there is one there too????

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password');
+        },
+
+        Mutation: {
+            addUser: async (parent, args) => {
+                const user = await User.create(args);
+                const token = signToken(user);
+                return { token, user };
+            }
+        }
+    }
+};
+
+module.exports = resolvers;
